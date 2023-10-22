a58a24ab49f48f85e16a14e053e8474fbf509e7d
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-10-22 11:25:28 +0200

    finished

---
+++ QuizModzelewski/app/src/main/java/pl/edu/pb/quizmodzelewski/MainActivity.java
@@ -0,0 +1,150 @@
+package pl.edu.pb.quizmodzelewski;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final String KEY_CURRENT_POINTS = "points";
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.quizmodzelewski.CorrectAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private Button answerButton;
+    private TextView questionTextView;
+    private Question[] questions = new Question[]{
+            new Question(R.string.q_activity, true),
+            new Question(R.string.q_find_resources, false),
+            new Question(R.string.q_listener, true),
+            new Question(R.string.q_resources, true),
+            new Question(R.string.q_version, false)
+    };
+    private int currentIndex = 0;
+    private int points = 0;
+    private boolean answerWasShown;
+    private void checkAnswerCorrectness(boolean userAnswer){
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
+        }else{
+            if(userAnswer == correctAnswer){
+                resultMessageId = R.string.correct_answer;
+                points++;
+            }else{
+                resultMessageId = R.string.incorrect_answer;
+            }
+        }
+        Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("call","onCreate called");
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState !=null)
+        {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        answerButton = findViewById(R.id.answer_Button);
+        questionTextView = findViewById(R.id.question_text_view);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(true);
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(false);
+            }
+        });
+        answerButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerWasShown = false;
+                if(currentIndex==4)
+                {
+                    questionTextView.setText("Zdobyłeś "+points+" punkty");
+                }
+                else{
+                    currentIndex = (currentIndex+1)%questions.length;
+                    setNextQuestion();
+                }
+
+            }
+        });
+        setNextQuestion();
+    }
+    private void setNextQuestion(){
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("call", "onStart called");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("call","onPause called");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("call","OnResume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("call","OnStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("call","OnDestroy called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d("call","Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+        outState.putInt(KEY_CURRENT_POINTS, points);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK) {
+            return ;
+        }
+        if(requestCode == REQUEST_CODE_PROMPT){
+            if(data == null) {
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+}
---
+++ QuizModzelewski/app/src/main/java/pl/edu/pb/quizmodzelewski/Question.java
@@ -0,0 +1,18 @@
+package pl.edu.pb.quizmodzelewski;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer) {
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+
+    public boolean isTrueAnswer() {
+        return trueAnswer;
+    }
+    public int getQuestionId(){
+        return questionId;
+    }
+}
